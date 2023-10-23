c8efbab13937a7cf82ec423eb97bf637d26665f2
Bean <bean@yuanfen.net>
2023-10-24 00:11:44 +0800

    feat!: change logger to class

--- yuanfen/logger.py
+++ yuanfen/logger.py
@@ -2,43 +2,41 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
-os.makedirs("logs", exist_ok=True)
 
-log_formatter = logging.Formatter("%(asctime)s [%(levelname)-8s] %(message)s")
+class Logger:
+    def __init__(self, logger_name):
+        os.makedirs("logs", exist_ok=True)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(log_formatter)
+        log_formatter = logging.Formatter("%(asctime)s [%(levelname)-8s] [%(name)-16s] %(message)s")
 
-file_handler = TimedRotatingFileHandler(
-    "logs/log",
-    when="midnight",
-    backupCount=365,
-    encoding="utf-8",
-)
-file_handler.suffix = "%Y-%m-%d.log"
-file_handler.setFormatter(log_formatter)
+        self.stream_handler = logging.StreamHandler()
+        self.stream_handler.setFormatter(log_formatter)
 
-logger = logging.getLogger()
-logger.setLevel(level=logging.INFO)
-logger.addHandler(stream_handler)
-logger.addHandler(file_handler)
+        self.file_handler = TimedRotatingFileHandler(
+            "logs/log",
+            when="midnight",
+            backupCount=365,
+            encoding="utf-8",
+        )
+        self.file_handler.suffix = "%Y-%m-%d.log"
+        self.file_handler.setFormatter(log_formatter)
 
+        self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(level=logging.INFO)
+        self.logger.addHandler(self.stream_handler)
+        self.logger.addHandler(self.file_handler)
 
-def set_level(level):
-    logger.setLevel(level)
+    def set_level(self, level):
+        self.logger.setLevel(level)
 
+    def debug(self, msg):
+        self.logger.debug(f"{msg}")
 
-def debug(msg):
-    logger.debug(f"{msg}")
+    def info(self, msg):
+        self.logger.info(f"{msg}")
 
+    def warn(self, msg):
+        self.logger.warning(f"{msg}")
 
-def info(msg):
-    logger.info(f"{msg}")
-
-
-def warn(msg):
-    logger.warn(f"{msg}")
-
-
-def error(msg):
-    logger.error(f"{msg}")
+    def error(self, msg):
+        self.logger.error(f"{msg}")
