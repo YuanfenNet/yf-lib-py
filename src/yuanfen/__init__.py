48f27f82fe7b7a67ff447b9bf8113bff147e827c
Bean <dengbin@bilibili.com>
2023-10-24 20:58:35 +0800

    chore: use hatch version

--- src/yuanfen/__init__.py
+++ src/yuanfen/__init__.py
@@ -1,9 +1,9 @@
 from .config import Config
 from .logger import Logger
 from .response import BaseResponse, SuccessResponse, ErrorResponse
-from .env import APP_ENV, VERSION
+from .env import APP_ENV
 
-__version__ = VERSION
+__version__ = "0.0.0"
 
 __all__ = [
     "APP_ENV",
--- src/yuanfen/env.py
+++ src/yuanfen/env.py
@@ -1,4 +1,3 @@
 import os
 
 APP_ENV = os.getenv("APP_ENV", "dev")
-VERSION = os.getenv("CI_COMMIT_TAG", "0.0.0")
