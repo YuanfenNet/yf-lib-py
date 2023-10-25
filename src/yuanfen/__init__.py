3f3b50188ddf493fcf0675db52acdc397a77d683
Bean <dengbin@bilibili.com>
2023-10-25 11:03:30 +0800

    fix

--- src/yuanfen/__init__.py
+++ src/yuanfen/__init__.py
@@ -2,7 +2,7 @@ from .config import Config
 from .logger import Logger
 from .response import BaseResponse, SuccessResponse, ErrorResponse
 from .env import APP_ENV
-import time
+from . import time
 
 __version__ = "0.0.0"
 
