7bd2bc6265dde47483eea9cb1b30536e5ca847a7
Bean <dengbin@bilibili.com>
2023-12-04 18:12:32 +0800

    feat: add Email

--- src/yuanfen/__init__.py
+++ src/yuanfen/__init__.py
@@ -1,5 +1,6 @@
 from . import time
 from .config import Config
+from .email import Email
 from .env import APP_ENV
 from .group_robot import GroupRobot
 from .logger import Logger
@@ -11,6 +12,7 @@ __all__ = [
     "APP_ENV",
     "BaseResponse",
     "Config",
+    "Email",
     "ErrorResponse",
     "GroupRobot",
     "Logger",
